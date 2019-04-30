505790ee01b7f265d1e067e4b1e87284c0b03271
Percy Maldonado Quispe <maldonadoq.percy@gmail.com>
2019-04-30 14:22:04 -0500

    clase jugador posicion-velocidad-piso

--- inc/jugador.h
+++ inc/jugador.h
@@ -1,13 +1,35 @@
 #ifndef _JUGADOR_H_
 #define _JUGADOR_H_
 
+#include <GL/glut.h>
+#include <glm/glm.hpp>
+#include <iostream>
+
 class TJugador{
-private:
-	glm::vec3 m_posicion;
+private:	
 public:
+	glm::vec3 m_posicion;	
+	glm::vec3 m_dim;	
+
+	glm::vec3 m_posicion_inicial;
+	glm::vec3 m_velocidad_inicial;
+
+	float m_piso;
+	float m_time;
+
+	bool m_saltar;
+	float m_mover;
+
+	TJugador(glm::vec3);
 	TJugador();
 	~TJugador();
 
+	void mover(glm::vec3);
+	void dibujar();
+
+	void set_posicion_inicial();
+
+	glm::vec3 get_posicion();
 };
 
 #endif
