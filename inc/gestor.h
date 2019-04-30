114f45e23789a1307a6129478bcf3c215a3b5535
Percy Maldonado Quispe <maldonadoq.percy@gmail.com>
2019-04-30 14:18:24 -0500

    manejador de las escenas!

---
+++ inc/gestor.h
@@ -0,0 +1,28 @@
+#ifndef _GESTOR_H_
+#define _GESTOR_H_
+
+#include <glm/glm.hpp>
+#include "jugador.h"
+#include "mapa.h"
+
+class TGestor{
+private:
+	TJugador *m_jugador;
+	TMapa *m_mapa;
+	glm::vec3 m_aceleracion;
+public:
+	TGestor();
+
+	void set_mapa(TMapa *);
+	void set_jugador(TJugador *);
+
+	void mover_jugador(glm::vec3);
+
+	void saltar_jugador(float);
+	void dibujar_mapa();
+	void dibujar_jugador(glm::vec3, float);
+
+	~TGestor();
+};
+
+#endif
---
+++ src/gestor.cpp
@@ -0,0 +1,60 @@
+#include "../inc/gestor.h"
+
+TGestor::TGestor(){
+	this->m_jugador = NULL;
+	this->m_mapa = NULL;
+	this->m_aceleracion = glm::vec3(0.0f, -9.81f, 0.0f);
+}
+
+void TGestor::set_mapa(TMapa *_mapa){
+	this->m_mapa = _mapa;
+}
+
+void TGestor::set_jugador(TJugador *_jugador){
+	this->m_jugador = _jugador;
+}
+
+void TGestor::dibujar_mapa(){
+	this->m_mapa->dibujar();
+}
+
+void TGestor::saltar_jugador(float _dt){
+	if(m_jugador->m_saltar){
+		m_jugador->m_time += _dt;
+
+		m_jugador->m_posicion = 
+			m_jugador->m_posicion_inicial +
+			(m_jugador->m_velocidad_inicial*m_jugador->m_time) + 
+			(m_aceleracion*(float)pow(m_jugador->m_time,2)*0.5f);
+
+		if(m_jugador->m_posicion.y <
+			(m_mapa->m_cuarto_actual->m_centro.y + m_jugador->m_piso)){
+			m_jugador->m_saltar = false;
+			m_jugador->m_time = 0;
+			m_jugador->m_posicion.y = m_jugador->m_piso;
+		}
+	}
+}
+
+void TGestor::dibujar_jugador(glm::vec3 _dir, float _dt){
+	if(m_jugador->m_mover){
+		mover_jugador(_dir*m_jugador->m_mover);
+	}
+
+	if(m_jugador->m_saltar){
+		saltar_jugador(_dt);
+	}
+
+	this->m_jugador->dibujar();
+}
+
+void TGestor::mover_jugador(glm::vec3 _dir){
+	glm::vec3 _pos = m_jugador->m_posicion+_dir;
+	if(m_mapa->m_cuarto_actual->colision_paredes(_pos)){
+		m_jugador->mover(_pos);
+	}	
+}
+
+TGestor::~TGestor(){
+	
+}
