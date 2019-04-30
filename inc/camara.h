9189047d4279baa0fc498acab7b6a925ab044cc5
Percy Maldonado Quispe <maldonadoq.percy@gmail.com>
2019-04-30 14:20:06 -0500

    actualizar - mover camara

--- inc/camara.h
+++ inc/camara.h
@@ -8,20 +8,38 @@ private:
 public:	
 	glm::vec4 m_perspective;	// fovy,ratio,znear,zfar
 	glm::vec3 m_posicion;		// x,y,z
+	glm::vec3 m_direccion;
+
+	float m_angulo;
+	float m_delta_mangle;
+	float m_delta_tangle;
 
 	TCamara(float _fovy, float _ratio,
 		float _znear, float _zfar){
 		this->m_perspective = glm::vec4(_fovy, _ratio, _znear, _zfar);
-		this->m_posicion = glm::vec3(50,20,50);
+		this->m_posicion = glm::vec3(0,0,5);
+		this->m_direccion = glm::vec3(0,0,-1);
+
+		this->m_angulo = 0.0f;
+		this->m_delta_tangle = 0.0f;	// tecla
+		this->m_delta_mangle = 0.0f;	// mouse
 	}
 
-	void setPosicion(glm::vec3 _pos){
+	void set_posicion(glm::vec3 _pos){
 		this->m_posicion = _pos;
 	}
 
 	TCamara(){
 		this->m_perspective = glm::vec4(0,0,0,0);
 		this->m_posicion = glm::vec3(0,0,0);
+		this->m_direccion = glm::vec3(0,0,0);
+	}
+
+	void actualizar(){
+		if(m_delta_tangle){
+			m_direccion.x = sin(m_angulo += m_delta_tangle);
+			m_direccion.z = -cos(m_angulo += m_delta_tangle);
+		}
 	}
 
 	~TCamara(){	};
