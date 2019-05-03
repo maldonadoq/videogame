22e213f740db0f6a1bb4ade3114862f3eab51b2f
Percy Maldonado Quispe <maldonadoq.percy@gmail.com>
2019-05-02 19:41:46 -0500

    textures folder!

--- src/mapa.cpp
+++ src/mapa.cpp
@@ -8,8 +8,8 @@
 TMapa::TMapa(){
 	this->m_cuarto_actual = new TCuarto(glm::vec3(0,-15,0), glm::vec3(100,30,100));
 	
-	this->texturas_id[0] = TextureManager::Inst()->LoadTexture("data/floor.jpg", GL_BGR_EXT, GL_RGB);
-	this->texturas_id[1] = TextureManager::Inst()->LoadTexture("data/walld.jpg", GL_BGR_EXT, GL_RGB);
+	this->texturas_id[0] = TextureManager::Inst()->LoadTexture("data/texturas/floor.jpg", GL_BGR_EXT, GL_RGB);
+	this->texturas_id[1] = TextureManager::Inst()->LoadTexture("data/texturas/walld.jpg", GL_BGR_EXT, GL_RGB);
 
 	// this->texturas_id[0] = TextureManager::Inst()->LoadTexture("data/granito.jpg", GL_BGR_EXT, GL_RGB);
 	// this->texturas_id[1] = TextureManager::Inst()->LoadTexture("data/brickt.jpg", GL_BGR_EXT, GL_RGB);
