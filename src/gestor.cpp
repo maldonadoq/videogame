2e26b5848a57a940caee49a3a1e5d6a0cfd0efc2
Jesamin-30 <jesaminzev@gmail.com>
2019-05-14 19:00:06 -0500

    update gestor.h

--- src/gestor.cpp
+++ src/gestor.cpp
@@ -14,6 +14,10 @@ void TGestor::set_jugador(TJugador *_jugador){
 	this->m_jugador = _jugador;
 }
 
+void TGestor::set_enemigo(TEnemigo *_enemigo){
+	this->m_enemigo= _enemigo;
+}
+
 void TGestor::dibujar_mapa(){
 	this->m_mapa->dibujar();
 }
@@ -75,11 +79,39 @@ void TGestor::dibujar_jugador(glm::vec3 _dir, float _dt){
 	this->m_jugador->dibujar();
 }
 
+void TGestor::dibujar_enemigo(glm::vec3 _dir,float _dt){
+	if(m_enemigo->m_mover){
+		mover_enemigo(_dir*m_enemigo->m_mover);
+	}
+}
+
 void TGestor::mover_jugador(glm::vec3 _dir){
 	glm::vec3 _pos = m_jugador->m_posicion+_dir;
 	if(m_mapa->m_cuarto_actual->colision_paredes(_pos)){
 		m_jugador->mover(_pos);
 	}	
+}
+
+void TGestor::mover_enemigo(glm::vec3 _dir){
+	glm::vec3 _pos = m_jugador->m_posicion+_dir;
+	glColor3f(1.0f, 1.0f, 1.0f);
+
+	//glTranslatef(0,-5,-20);//posición
+	glTranslatef(m_jugador->m_posicion.x,m_jugador->m_posicion.y,m_jugador->m_posicion.z-50);
+	//cuerpo	
+	glTranslatef(0.0f ,0.0f, 0.0f);
+	glutSolidSphere(4,20,20);
+
+	//cabeza
+	glTranslatef(0.0f, 5.5f, 0.0f);
+	glutSolidSphere(2,20,20);
+
+	//nariz
+	glColor3f(1.0f, 0.5f , 0.5f);
+	glTranslatef(0.0f, 0.0f, 2.0f);
+	glRotatef(0.0f,1.0f, 0.0f, 0.0f);
+	glutSolidCone(0.5f, 2.0f,10,2);
+
 }
 
 TGestor::~TGestor(){
