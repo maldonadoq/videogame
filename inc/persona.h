0a64abd9561c998165d01fd45dee91b140c2acd7
Percy Maldonado Quispe <maldonadoq.percy@gmail.com>
2019-05-16 22:35:08 -0500

    persona clas

--- inc/persona.h
+++ inc/persona.h
@@ -4,16 +4,22 @@
 #include <glm/glm.hpp>
 #include <vector>
 #include "bala.h"
+#include "utils.h"
 
 class TPersona{
+protected:
+	TMaterial m_material;
 public:
+	glm::vec3 m_direccion;
+
     std::vector<TBala> m_balas;
-	
-	glm::vec3 m_posicion;	
-	glm::vec3 m_dim;	
+	glm::vec3 m_posicion;
+	glm::vec3 m_dim;
 
 	glm::vec3 m_posicion_inicial;
 	glm::vec3 m_velocidad_inicial;
+
+	glm::vec3 m_color;
 
 	float m_piso;
 	float m_time;
@@ -39,6 +45,11 @@ public:
 	virtual void dibujar() = 0;
 	virtual void set_posicion_inicial() = 0;
 	virtual glm::vec3 get_posicion() = 0;
+
+	friend std::ostream& operator<< (std::ostream & out, const TPersona &p){
+       out << "[" << p.m_posicion.x << "," << p.m_posicion.y << "," << p.m_posicion.z << "]";
+       return out;
+   	}
 };
 
 #endif
