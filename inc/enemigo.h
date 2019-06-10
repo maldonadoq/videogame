e641b61f6535ab6cefa389a8adf5ea583096940b
Unknown <percy.maldonado@ucsp.edu.pe>
2019-06-09 23:02:27 -0500

    Clase Abstracta Pura

--- inc/enemigo.h
+++ inc/enemigo.h
@@ -1,32 +1,51 @@
 #ifndef _ENEMIGO_H_
 #define _ENEMIGO_H_
 
-#include <GL/glut.h>
 #include <glm/glm.hpp>
 #include <glm/gtc/type_ptr.hpp>
-
-#include <iostream>
 #include <vector>
 #include "bala.h"
-#include "persona.h"
+#include "utils.h"
+#include "modelo.h"
 #include "random.h"
 
-class TEnemigo: public TPersona{
-private:
-  float m_tiempo_disparar;
-public:	
-    TEnemigo(glm::vec3);
-    TEnemigo();
-    ~TEnemigo();
+class TEnemigo{
+protected:
+	float m_tiempo_disparar;
+public:
+	TModelo * m_modelo;
+	glm::vec3 m_direccion;
 
-	void mover(glm::vec3);
+    std::vector<TBala> m_balas;
+	
+	glm::vec3 m_posicion_inicial;
+	glm::vec3 m_velocidad;
+	glm::vec3 m_posicion;
+
+    int m_vida;
+    
+    TEnemigo(glm::vec3 _pos);
+	TEnemigo();
+    virtual ~TEnemigo();
+	
 	void anhadir_bala(TBala);
-
-	void dibujar();
-	void set_posicion_inicial();
-	glm::vec3 get_posicion();
-	void disparar(float, glm::vec3);
 	void barra_vida();
+	void restart();
+	void cargar(float);					// espera para disparar de nuevo
+	void set_position(glm::vec3);
+
+	void dibujar(glm::vec3, glm::vec3);
+	void dibujar_balas(glm::vec3, glm::vec3);
+	int  colision(glm::vec3, float);
+
+	virtual glm::vec3 mover(glm::vec3, glm::vec3, float) = 0;
+	// virtual void disparar(glm::vec3) = 0;
+	virtual void disparar(glm::vec3, float) = 0;
+
+	friend std::ostream& operator<< (std::ostream & out, const TEnemigo &p){
+       out << "[" << p.m_posicion.x << "," << p.m_posicion.y << "," << p.m_posicion.z << "]";
+       return out;
+   	}
 };
 
 #endif
