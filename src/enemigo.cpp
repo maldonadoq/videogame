f51644b6e08050de8917165383afbae2ee47946b
Unknown <percy.maldonado@ucsp.edu.pe>
2019-06-09 23:05:57 -0500

    enemigo

--- src/enemigo.cpp
+++ src/enemigo.cpp
@@ -6,83 +6,75 @@ y disparar.
 
 #include "../inc/enemigo.h"
 
-TEnemigo::TEnemigo(){
-
+TEnemigo::TEnemigo(glm::vec3 _pos){
+	this->m_posicion = _pos;
 }
 
-TEnemigo::TEnemigo(glm::vec3 _pos): TPersona(_pos){
-	this->m_velocidad = glm::vec3(0.0f, 5.0f, 0.0f);
-    this->m_vida = 5;
-	this->m_tiempo_disparar = 0;
-    this->m_material = {
-		glm::vec3(0.0f,0.2f,0.2f),
-		RandomColor(),
-		glm::vec3(1.0f,1.0f,1.0f),
-		0.6f
-	};
-
-
-	this->m_direccion = 10.0f*RandomVect();	
-
-	this->m_color = RandomColor();
-
-	// std::cout << "[" << m_color.x << "," << m_color.y << "," << m_color.z << "]\n";
-}
-
-TEnemigo::~TEnemigo(){
-}
-
-void TEnemigo::mover(glm::vec3 _pos){
-	m_posicion = _pos;
-}
-
-void TEnemigo::dibujar(){
-	glMaterialfv(GL_FRONT_AND_BACK, GL_AMBIENT  , glm::value_ptr(m_material.m_ambient));
-	glMaterialfv(GL_FRONT_AND_BACK, GL_DIFFUSE  , glm::value_ptr(m_material.m_diffuse));
-	glMaterialfv(GL_FRONT_AND_BACK, GL_SPECULAR , glm::value_ptr(m_material.m_specular));
-	glMaterialf(GL_FRONT_AND_BACK , GL_SHININESS, m_material. m_shininess);
-
-	glPushMatrix();		
+void TEnemigo::dibujar(glm::vec3 _dim, glm::vec3 _centro){
+	dibujar_balas(_dim, _centro);
+	glPushMatrix();
 		glTranslatef(m_posicion.x, m_posicion.y, m_posicion.z);
-		
-		//cuerpo
-		//glColor3f(m_color.x, m_color.y, m_color.z);
-		glTranslatef(0.0f ,0.0f, 0.0f);
-		glutSolidSphere(4,20,20);
+		glLineWidth(3);
+		glColor3f(0.0f, 1.0f, 0.0f);
 
-		//cabeza
-		glTranslatef(0.0f, 5.5f, 0.0f);
-		glutSolidSphere(2,20,20);
+		glutWireSphere(m_modelo->m_dim, 8, 8);
+		// glutSolidSphere(m_dim, 8, 8);
 
-		//nariz
-		//glColor3f(1.0f, 0.5f , 0.5f);
-		glTranslatef(0.0f, 0.0f, 2.0f);
-		glRotatef(0.0f,1.0f, 0.0f, 0.0f);
-		glutSolidCone(0.5f, 2.0f,10,2);
-
+		// glScalef(0.2f, 0.2f, 0.2f);
+		// glRotatef(-90, 1, 0, 0);
+		m_modelo->dibujar();
 	glPopMatrix();
-}
-
-void TEnemigo::set_posicion_inicial(){
-	this->m_posicion_inicial = m_posicion;
-}
-
-glm::vec3 TEnemigo::get_posicion(){
-	return this->m_posicion;
 }
 
 void TEnemigo::anhadir_bala(TBala _bala){
 	this->m_balas.push_back(_bala);
 }
 
-void TEnemigo::disparar(float _dt, glm::vec3 _dir){
-	this->m_tiempo_disparar += _dt;
+void TEnemigo::cargar(float _dt){
+	m_tiempo_disparar += _dt;
+}
 
-	if(m_tiempo_disparar > 2.5f){
-		TBala tb = {0.4f, m_posicion, _dir};
-		m_balas.push_back(tb);
-		m_tiempo_disparar = 0;
+void TEnemigo::dibujar_balas(glm::vec3 _dim, glm::vec3 _centro){
+	int i;
+	// std::cout << m_balas.size() << "| ";
+	glColor3f(0.0f, 1.0f, 0.0f);
+	for(i=0; i<(int)m_balas.size(); i++){
+		glPushMatrix();
+			glTranslatef(
+				m_balas[i].m_posicion.x,
+				m_balas[i].m_posicion.y,
+				m_balas[i].m_posicion.z
+			);
+
+			glutSolidSphere(m_balas[i].m_radio,8,8);
+		glPopMatrix();
+		m_balas[i].m_posicion += m_balas[i].m_direccion;
+
+		if(!cuarto_colision(_dim, _centro, m_balas[i].m_posicion)){
+			m_balas.erase(m_balas.begin() + i);
+			i--;
+		}
 	}
+}
+
+int TEnemigo::colision(glm::vec3 _pos, float _r){
+	int ty = -1;
+
+	float dis = glm::distance(_pos, m_posicion);
+	//std::cout << dis << "," << (_r+m_dim) << "\t|";
+
+	if(dis < (m_modelo->m_dim + _r)){
+		ty = 1;
+		m_vida--;
+
+		if(m_vida <= 0){
+			ty = 2;
+		}
+	}
+
+	// std::cout << ty << " ";
+
+	return ty;
 }
 
 void TEnemigo::barra_vida(){
@@ -138,4 +130,16 @@ void TEnemigo::barra_vida(){
 		glEnd();  // End of drawing color-cube
 		
 	glPopMatrix();
+}
+
+void TEnemigo::set_position(glm::vec3 _pos){
+	this->m_posicion = _pos;
+}
+
+void TEnemigo::restart(){
+	m_direccion = 10.0f*RandomVect();
+}
+
+TEnemigo::~TEnemigo(){
+	
 }
